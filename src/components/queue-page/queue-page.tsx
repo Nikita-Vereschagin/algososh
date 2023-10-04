adeab9e5fa32a8fb9086bad0bc833c70cc5f4ed9
Nikita-Vereschagin <vereshagin21122006nikita@gmail.com>
2023-10-04 08:50:46 +0700

    queue works

--- src/components/queue-page/queue-page.tsx
+++ src/components/queue-page/queue-page.tsx
@@ -12,8 +12,8 @@ import { addTimeOut } from "../../tools/tools";
 type TArr = TSwap | {number: undefined, state: undefined}
 
 export const QueuePage: React.FC = () => {
-  let tail: number | null = null
-  let head: number | null = null
+  const [index, setIndex] = useState<{tail: number, head: number | null}>({tail: 0, head: null})
+  const [canDel, setCanDel] = useState(true)
   let size = 7
   const { values, handleChange, setValues } = useForm({input: ''})
   const [flag, setFlag] = useState(false)
@@ -22,54 +22,72 @@ export const QueuePage: React.FC = () => {
   const delEl = async() => {
     setFlag(true)
     let modifiedArray: TArr[] = output
-    if (head !== null && modifiedArray[head].number &&  modifiedArray[head].state && head < modifiedArray.length) {
-      modifiedArray[head] = {number: undefined, state: undefined}
-      if (head === modifiedArray.length - 1 || head === tail) tail = null
-      head < modifiedArray.length - 1 && head++
+    if (index.head !== null){
+      if (modifiedArray[index.head+1].number === undefined){
+        modifiedArray[index.head] = {number: Number(values.input), state: ElementStates.Changing}
+        setOutput([...modifiedArray])
+        await addTimeOut(500)
+        modifiedArray[index.head] = {number: undefined, state: undefined}
+        setOutput([...modifiedArray])
+        setCanDel(true)
+      }else{
+        modifiedArray[index.head] = {number: Number(values.input), state: ElementStates.Changing}
+        setOutput([...modifiedArray])
+        await addTimeOut(500)
+        modifiedArray[index.head] = {number: undefined, state: undefined}
+        setIndex({...index, head: index.head + 1})
+        setOutput([...modifiedArray])
+      }
     }
+
+
     setFlag(false)
     modifiedArray = []
   }
 
   const submit: FormEventHandler = async(e) => {
     e.preventDefault()
-    let modifiedArray: TArr[] = output
     setFlag(true)
-    if (head === null && tail === null) {
-      head = 0
-      tail = 0
-    }
-    if (tail !== null && head !== null && tail < modifiedArray.length -1) {
-        tail++
-        modifiedArray[tail] = {number: Number(values.input), state: ElementStates.Changing}
+    let modifiedArray: TArr[] = output
+    if (index.head === null){
+
+      setCanDel(false)
+      modifiedArray[index.tail] = {number: Number(values.input), state: ElementStates.Changing}
+      setOutput([...modifiedArray])
+      await addTimeOut(500)
+      modifiedArray[index.tail] = {number: Number(values.input), state: ElementStates.Default}
+      setIndex({head: 0, tail: index.tail+1})
+      setOutput([...modifiedArray])
+    }else{
+      if (index.tail < output.length){
+        setCanDel(false)
+        modifiedArray[index.tail] = {number: Number(values.input), state: ElementStates.Changing}
         setOutput([...modifiedArray])
         await addTimeOut(500)
-        modifiedArray[tail].state = ElementStates.Default
+        modifiedArray[index.tail] = {number: Number(values.input), state: ElementStates.Default}
+        setIndex({...index, tail: index.tail+1})
         setOutput([...modifiedArray])
-      } else if (tail === null && head !== null && head > 0 && head !== modifiedArray.length - 1) {
-        modifiedArray[head] = {number: Number(values.input), state: ElementStates.Changing}
-        setOutput([...modifiedArray])
-        await addTimeOut(500)
-        modifiedArray[head].state = ElementStates.Default
-        setOutput([...modifiedArray])
-        tail = head
-        
+      } else{
+        setCanDel(true)
       }
+  
+    }
+    console.log(index.head)
     setFlag(false)
     setValues({input: ''})
     modifiedArray = []
   };
-  console.log(head, tail)
+
   return (
     <SolutionLayout title="Очередь">
       <form className={style.box} onSubmit={submit}>
-        <Input max={4} maxLength={4} isLimitText={true} name="input" type={typeof values.input} value={values.input} onChange={handleChange}/>
-        <Button text="Добавить" isLoader={flag} type="submit" disabled={isNaN(Number(values.input)) || values.input === '' ? true : false}/>
-        <Button isLoader={flag} text="Удалить" type="button" disabled={output?.length === 0 || output === undefined} onClick={()=>delEl()}/>
-        <Button text="Очистить" style={{marginLeft: 80}} type="button" disabled={output?.length === 0 || output === undefined} onClick={()=>setOutput([...Array(size).fill({number: undefined, state: undefined})])}/>
+        <Input disabled={index.tail === output.length} max={4} maxLength={4} isLimitText={true} name="input" type={typeof values.input} value={values.input} onChange={handleChange}/>
+        <Button text="Добавить" isLoader={flag} type="submit" disabled={isNaN(Number(values.input)) || values.input === ''}/>
+        <Button isLoader={flag} text="Удалить" type="button" disabled={canDel || index.tail === output.length} onClick={()=>delEl()}/>
+        <Button text="Очистить" disabled={canDel || index.tail === output.length} style={{marginLeft: 80}} type="button"  onClick={()=>{setOutput([...Array(size).fill({number: undefined, state: undefined})]);setIndex({tail: 0, head: 0});setCanDel(true)}}/>
       </form>
       <div className={style.circles}>
-        {output && output?.map((el: TArr, i: number) => {return <Circle letter={el?.number?.toString()} tail={i === tail ? "tail" : ""} head={i === head ? "top" : ""} extraClass={style.margin} key={i} state={el?.state}/>})}
+        {output && output?.map((el: TArr, i: number) => {return <Circle letter={el?.number?.toString()} tail={i + 1 === index.tail  && el.number ? "tail" : ""} head={i === index.head  ? "top" : ""} index={i} extraClass={style.margin} key={i} state={el?.state}/>})}
       </div>
     </SolutionLayout>
   );
